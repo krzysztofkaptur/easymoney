de50c6dcbbd73b103017bd14600325f638037036
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-07-09 16:41:41 +0200

    feat: Add FAQ component

--- src/app/[lang]/(pages)/(home)/page.tsx
+++ src/app/[lang]/(pages)/(home)/page.tsx
@@ -1,7 +1,7 @@
 import { db } from '@/lib/db'
 import { todos } from '@/lib/db'
 import { getDictionary } from '@/lib/i18n/get-dictionary'
-import { Text, Image, Modal } from '@/app/[lang]/components'
+import { Text, Image, Modal, FAQ } from '@/app/[lang]/components'
 
 import type { Locale } from '@/lib/i18n'
 
@@ -20,6 +20,7 @@ export default async function Home({ params: { lang } }: Props) {
       <Image src='/images/next.svg' alt='' width={100} height={20} />
       <Text variant='h1'>{t.home.title}</Text>
       <Modal />
+      <FAQ />
     </main>
   )
 }
