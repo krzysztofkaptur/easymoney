648570c3b7cf3e68e807d6eda45a8f2a132e3701
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-07-11 12:24:46 +0200

    feat: Customize InputGroup component

--- src/app/[lang]/(pages)/(home)/page.tsx
+++ src/app/[lang]/(pages)/(home)/page.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenu,
   FAQ,
   Image,
+  InputGroup,
   Modal,
   Pagination,
   Select,
@@ -40,11 +41,13 @@ export default async function Home({ params: { lang } }: Props) {
 
   return (
     <main>
-      <Badge text='badge' />
+      <InputGroup name='test' label='Test' />
       {/* <ThemeToggle />
       <Avatar src='https://github.com/shadcn.png' alt='@shadcn' fallback='CN' />
+      <Badge text='badge' />
       <Image src='/images/next.svg' alt='' width={100} height={20} />
       <Text variant='h1'>{t.home.title}</Text>
+      <Card />
       <Modal />
       <FAQ />
       <Checkbox />
@@ -53,7 +56,6 @@ export default async function Home({ params: { lang } }: Props) {
       <Pagination />
       <Select />
       <Drawer />
-      <Card />
       <Textarea />
       <Switch />
       <Tabs />
